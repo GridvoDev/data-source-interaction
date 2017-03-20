5b738f450bb6e6eff2badd650a04ed659ccfaf05
linmadan <772181827@qq.com>
2017-03-21 00:23:56 +0800

    完成数据源接入与控制流程

--- test/util/parseTest.js
+++ test/util/parseTest.js
@@ -6,7 +6,7 @@ const {
     parseDataSourceIDFromTopic,
     parseActionFromTopic,
     parseConfigUpdateResultFromMqttData,
-    parseDataSourceUpdateMqttTopicFromKafka
+    parseDataSourceConfigMqttTopicFromKafka
 } = require('../../lib/util/parse');
 
 describe('parse util unit test', () => {
@@ -14,7 +14,7 @@ describe('parse util unit test', () => {
         context('parse data source id from topic', () => {
             let topic = "/rt/GF/FZMHQJT/FWJ/publish";
             let dataSource = parseDataSourceIDFromTopic(topic);
-            dataSource.should.eql("FZMHQJT-FWJ");
+            dataSource.should.eql("GF-FZMHQJT-FWJ");
         });
     });
     describe('parseActionFromTopic(topic)', () => {
@@ -32,7 +32,7 @@ describe('parse util unit test', () => {
                     payload: JSON.stringify({v: "body", t: 1})
                 }
                 let originalData = parseOriginalDataFromMqttData(mqttData);
-                originalData.s.should.eql("FZMHQJT-FWJ");
+                originalData.s.should.eql("GF-FZMHQJT-FWJ");
                 originalData.v.should.eql("body");
                 originalData.t.should.eql(1);
             });
@@ -46,19 +46,19 @@ describe('parse util unit test', () => {
                     payload: JSON.stringify({v: "ok", t: 1})
                 }
                 let updateResult = parseConfigUpdateResultFromMqttData(mqttData);
-                updateResult.s.should.eql("FZMHQJT-FWJ");
+                updateResult.s.should.eql("GF-FZMHQJT-FWJ");
                 updateResult.v.should.eql("ok");
                 updateResult.t.should.eql(1);
             });
         });
     });
-    describe('parseDataSourceUpdateMqttTopicFromKafka(dataSourceConfigTopicMessage)', () => {
+    describe('parseDataSourceConfigMqttTopicFromKafka(dataSourceConfigTopicMessage)', () => {
         context('parse mqtt topic from kafka message', () => {
             it('is ok', () => {
                 let dataSourceConfigTopicMessage = {
                     dataSourceID: "GF-FZMHQJT-FWJ"
                 }
-                let mqttTopic = parseDataSourceUpdateMqttTopicFromKafka(dataSourceConfigTopicMessage);
+                let mqttTopic = parseDataSourceConfigMqttTopicFromKafka(dataSourceConfigTopicMessage);
                 mqttTopic.should.eql("/rt/GF/FZMHQJT/FWJ/config");
             });
         });
